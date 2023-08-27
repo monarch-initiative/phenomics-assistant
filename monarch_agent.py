f3de2fb4c9f6940b8c5461dd2f894276edbd5fad
Shawn O'Neil <oneilsh@gmail.com>
2023-08-26 20:18:24 -0700

    first attempt at utilityagent integration

---
+++ monarch_agent.py
@@ -0,0 +1,47 @@
+from agent_smith_ai.utility_agent import UtilityAgent
+
+import textwrap
+import os
+from typing import Any, Dict
+
+
+
+## A UtilityAgent can call API endpoints and local methods
+class MonarchAgent(UtilityAgent):
+
+    def __init__(self, name):
+        
+        ## define a system message
+        system_message = textwrap.dedent(f"""
+            You are the Monarch Assistant, an AI-powered chatbot that can answer questions about data from the Monarch Initiative knowledge graph. 
+            You can search for entities such as genes, diseases, and phenotypes by name to get the associated ontology identifier. 
+            You can retrieve associations between entities via their identifiers. 
+            Users may use synonyms such as 'illness' or 'symptom'. Do not assume the user is familiar with biomedical terminology. 
+            Always add additional information such as lay descriptions of phenotypes. 
+            If the user changes the show function call setting, do not make any further function calls immediately.
+            IMPORTANT: Include markdown-formatted links to the Monarch Initiative for all results using the templates provided by function call responses.'.
+            """).strip()
+        
+        super().__init__(name,                                             # Name of the agent
+                         system_message,                                   # Openai system message
+                         model = "gpt-3.5-turbo-0613",                     # Openai model name
+                         openai_api_key = os.environ["OPENAI_API_KEY"],    # API key; will default to OPENAI_API_KEY env variable
+                         auto_summarize_buffer_tokens = 500,               # Summarize and clear the history when fewer than this many tokens remains in the context window. Checked prior to each message sent to the model.
+                         summarize_quietly = False,                        # If True, do not alert the user when a summarization occurs
+                         max_tokens = None,                                # maximum number of tokens this agent can bank (default: None, no limit)
+                         token_refill_rate = 10000.0 / 3600.0)             # number of tokens to add to the bank per second
+
+        ## register some API endpoints (inherited from UtilityAgent)
+        ## the openapi.json spec must be available at the spec_url:
+        ##    callable endpoints must have a "description" and "operationId"
+        ##    params can be in body or query, but must be fully specified
+        self.register_api("monarch", 
+                          spec_url = "https://oai-monarch-plugin.monarchinitiative.org/openapi.json", 
+                          base_url = "https://oai-monarch-plugin.monarchinitiative.org",
+                          callable_endpoints = ['search_entity', 
+                                                'get_disease_gene_associations', 
+                                                'get_disease_phenotype_associations', 
+                                                'get_gene_disease_associations', 
+                                                'get_gene_phenotype_associations', 
+                                                'get_phenotype_gene_associations', 
+                                                'get_phenotype_disease_associations'])
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,28 @@
 import streamlit as st
+from monarch_agent import MonarchAgent
+
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
+agent = MonarchAgent("Monarch Agent")
+
 st.title("Monarch Agent") 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}, 
-                                    {"role": "user", "content": "I need help with my account."},
-                                    {"role": "function", "content": "1.2314"},
-                                    {"role": "user", "content": "ugh. ok."}
-                                    ]
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
     print("hey that tickles")
-    # if not openai_api_key:
+    # if not openai_api_key: 
     #     st.info("Please add your OpenAI API key to continue.")
     #     st.stop()
 
     # openai.api_key = openai_api_key
-    # st.session_state.messages.append({"role": "user", "content": prompt})
-    # st.chat_message("user").write(prompt)
-    # response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    # msg = response.choices[0].message
-    # st.session_state.messages.append(msg)
-    # st.chat_message("assistant").write(msg.content)
+    for message in agent.new_chat(prompt):
+        st.session_state.messages.append(message.model_dump())
+        st.chat_message(message["role"]).write(message["content"])
