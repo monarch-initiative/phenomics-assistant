3e1fb804e06cf9b01700c3d1e953f022776a8583
Shawn O'Neil <oneilsh@gmail.com>
2023-08-30 07:38:13 -0600

    removed attempts at ui-side token counting for now

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import os
 from monarch_agent import MonarchAgent
-from token_bucket import TokenBucketManager
 import dotenv
 
 
@@ -13,18 +12,15 @@ def initialize_session_state():
     st.session_state.setdefault("show_function_calls", False)
     st.session_state.setdefault("openai_api_key", os.environ["OPENAI_API_KEY"])
     st.session_state.setdefault("current_agent_name", "GPT-3.5 16k Agent")
-    st.session_state.setdefault("buckets", TokenBucketManager())
     st.session_state.setdefault("ui_disabled", False)
 
     if "agents" not in st.session_state:
         st.session_state.agents = {
             "GPT-3.5 16k Agent": {
                 "agent": MonarchAgent("Monarch Agent", model="gpt-3.5-turbo-16k-0613", openai_api_key=st.session_state.openai_api_key),
-                "bucket": "GPT-3.5 16k"
             },
             "GPT-4 Agent": {
                 "agent": MonarchAgent("Monarch Agent", model="gpt-4-0613", openai_api_key=st.session_state.openai_api_key),
-                "bucket": "GPT-4"
             }
         }
     
@@ -34,11 +30,6 @@ def initialize_session_state():
         if "messages" not in agent:
             agent["messages"] = []
     
-    if "buckets" not in st.session_state:
-        st.session_state.buckets = TokenBucketManager()
-        st.session_state.buckets.add_bucket("GPT-3.5 16k", 100000, 0)
-        st.session_state.buckets.add_bucket("GPT-4", 10000, 0)
-
 
 # Render chat message
 def render_message(message):
@@ -63,12 +54,7 @@ def render_message(message):
 def handle_chat_input():
     if prompt := st.chat_input(disabled = st.session_state.ui_disabled):
         agent = st.session_state.agents[st.session_state.current_agent_name]
-        bucket = st.session_state.buckets.get_bucket(agent['bucket'])
 
-        check_bucket = bucket.consume(st.session_state.agents[st.session_state.current_agent_name]['agent'].compute_token_cost(prompt))
-        if not check_bucket:
-            st.error("You have run out of tokens for this agent.")
-            return
 
         # Continue with conversation
         if not agent.get('conversation_started', False):
@@ -78,8 +64,9 @@ def handle_chat_input():
             messages = agent['agent'].continue_chat(prompt, yield_prompt_message=True)
 
         for message in messages:
-            agent['messages'].append(message)
-            render_message(message)
+           agent['messages'].append(message)
+           render_message(message)
+
 
 # Main Streamlit UI
 def main():
@@ -88,11 +75,6 @@ def main():
         agent_names = list(st.session_state.agents.keys())
         current_agent_name = st.selectbox("Base Model", options=agent_names, key="current_agent_name")
 
-        print(st.session_state.agents[current_agent_name]['bucket'])
-        print(st.session_state.buckets.get_bucket('GPT-3.5 16k'))
-        
-        bucket_tokens = st.session_state.buckets.get_bucket(st.session_state.agents[current_agent_name]['bucket']).tokens
-        st.text(f"Agent Tokens: {bucket_tokens:.0f}")
         #st.text_input("OpenAI API Key", key="user_api_key", type="password") # not complete
         st.checkbox("Show function calls", key="show_function_calls", disabled = st.session_state.ui_disabled)
 
