66e38249b8b194a58d6e069e8698eaca41f2104b
Shawn T O'Neil <oneilsh@gmail.com>
2024-05-28 21:14:03 -0700

    migrate to kani backend

--- agents.py
+++ agents.py
@@ -1,18 +1,16 @@
-from agent_smith_ai.utility_agent import UtilityAgent
-
+from kani_utils.kanis import StreamlitKani
+from kani import AIParam, ai_function
+from typing import Annotated, List
 import textwrap
-import os
-from typing import Any, Dict
+import logging
+import requests
 
 
+class PhenomicsAgent(StreamlitKani):
+    """A Kani that can use the redline library to compute diffs between text inputs."""
 
-## A UtilityAgent can call API endpoints and local methods
-class PhenomicsAgent(UtilityAgent):
-
-    def __init__(self, name, model = "gpt-3.5-turbo-16k-0613", openai_api_key = None, auto_summarize_buffer_tokens = 500):
-        
-        ## define a system message
-        system_message = textwrap.dedent(f"""
+    def __init__(self, *args, **kwargs):
+        kwargs['system_prompt'] = textwrap.dedent(f"""
             You are the Phenomics Assistant, an AI-powered chatbot that can answer questions about data from the Monarch Initiative knowledge graph. 
             You can search for entities such as genes, diseases, and phenotypes by name to get the associated ontology identifier. 
             You can retrieve associations between entities via their identifiers. 
@@ -21,26 +19,92 @@ class PhenomicsAgent(UtilityAgent):
             IMPORTANT: Include markdown-formatted links to the Monarch Initiative for all results using the templates provided by function call responses, AND include links to publications if provided.
             """).strip()
         
-        super().__init__(name,                                             # Name of the agent
-                         system_message,                                   # Openai system message
-                         model = model,                     # Openai model name
-                         openai_api_key = openai_api_key,    # API key; will default to OPENAI_API_KEY env variable
-                         auto_summarize_buffer_tokens = auto_summarize_buffer_tokens,               # Summarize and clear the history when fewer than this many tokens remains in the context window. Checked prior to each message sent to the model.
-                         summarize_quietly = False,                        # If True, do not alert the user when a summarization occurs
-                         max_tokens = None,                                # maximum number of tokens this agent can bank (default: None, no limit)
-                         token_refill_rate = 50000.0 / 3600.0)             # number of tokens to add to the bank per second
+        super().__init__(*args, **kwargs)
 
-        ## register some API endpoints (inherited from UtilityAgent)
-        ## the openapi.json spec must be available at the spec_url:
-        ##    callable endpoints must have a "description" and "operationId"
-        ##    params can be in body or query, but must be fully specified
-        self.register_api("monarch", 
-                          spec_url = "https://oai-monarch-plugin.monarchinitiative.org/openapi.json", 
-                          base_url = "https://oai-monarch-plugin.monarchinitiative.org",
-                          callable_endpoints = ['search_entity', 
-                                                'get_disease_gene_associations', 
-                                                'get_disease_phenotype_associations', 
-                                                'get_gene_disease_associations', 
-                                                'get_gene_phenotype_associations', 
-                                                'get_phenotype_gene_associations', 
-                                                'get_phenotype_disease_associations'])
+        self.greeting = "Hello! I am the Phenomics Assistant. How can I help you today?"
+        self.description = "Searches the Monarch Initiative knowledge graph." # "An AI-driven copy editor."
+        self.avatar = "ℹ️"
+        self.user_avatar = "👤"
+        self.name = "" # "RedLiner"
+
+        self.api_base = "https://oai-monarch-plugin.monarchinitiative.org"
+
+    # helper function to make API calls
+    def api_call(self, endpoint, timeout=30, **kwargs):
+        url = f"{self.api_base}/{endpoint}"
+        logging.info(f"############ api_call: url={url}, kwargs={kwargs}")
+        response = requests.get(url, params=kwargs, timeout=timeout)
+        response.raise_for_status()
+        return response.json()
+
+    # search endpoint, implements all parameters described in openapi.json
+    @ai_function()
+    def search_entity(self,
+                        term: Annotated[str, AIParam(desc="The ontology term to search for.")],
+                        category: Annotated[str, AIParam(desc="A single category to search within as a string. Valid categories are: biolink:Disease, biolink:PhenotypicQuality, and biolink:Gene")] = "biolink:Disease",
+                        limit: Annotated[int, AIParam(desc="The maximum number of search results to return.")] = 10,
+                        offset: Annotated[int, AIParam(desc="Offset for pagination of results.")] = 0):
+            """Search for entities in the Monarch knowledge graph."""
+            logging.info(f"search: term={term}, category={category}, limit={limit}, offset={offset}")
+            return self.api_call("search", term=term, category=category, limit=limit, offset=offset)
+    
+    # get disease gene associations
+    @ai_function()
+    def get_disease_gene_associations(self,
+                                        disease_id: Annotated[str, AIParam(desc="The ontology identifier of the disease.")],
+                                        limit: Annotated[int, AIParam(desc="The maximum number of associations to return.")] = 10,
+                                        offset: Annotated[int, AIParam(desc="Offset for pagination of results.")] = 0):
+            """Get a list of genes associated with a disease."""
+            return self.api_call("disease-genes", disease_id=disease_id, limit=limit, offset=offset)  
+
+    # get disease phenotype associations
+    @ai_function()
+    def get_disease_phenotype_associations(self,
+                                            disease_id: Annotated[str, AIParam(desc="The ontology identifier of the disease.")],
+                                            limit: Annotated[int, AIParam(desc="The maximum number of associations to return.")] = 10,
+                                            offset: Annotated[int, AIParam(desc="Offset for pagination of results.")] = 0):
+            """Get a list of phenotypes associated with a disease."""
+            return self.api_call("disease-phenotypes", disease_id=disease_id, limit=limit, offset=offset)
+    
+    # get gene disease associations
+    @ai_function()
+    def get_gene_disease_associations(self,
+                                        gene_id: Annotated[str, AIParam(desc="The identifier of the gene.")],
+                                        limit: Annotated[int, AIParam(desc="The maximum number of associations to return.")] = 10,
+                                        offset: Annotated[int, AIParam(desc="Offset for pagination of results.")] = 0):
+            """Get a list of diseases associated with a gene."""
+            return self.api_call("gene-diseases", gene_id=gene_id, limit=limit, offset=offset)
+    
+    # get gene phenotype associations
+    @ai_function()
+    def get_gene_phenotype_associations(self,
+                                        gene_id: Annotated[str, AIParam(desc="The ontology identifier of the gene.")],
+                                        limit: Annotated[int, AIParam(desc="The maximum number of associations to return.")] = 10,
+                                        offset: Annotated[int, AIParam(desc="Offset for pagination of results.")] = 0):
+            """Get a list of phenotypes associated with a gene."""
+            return self.api_call("gene-phenotypes", gene_id=gene_id, limit=limit, offset=offset)
+    
+    # get phenotype disease associations
+    @ai_function()
+    def get_phenotype_disease_associations(self,
+                                            phenotype_id: Annotated[str, AIParam(desc="The ontology identifier of the phenotype.")],
+                                            limit: Annotated[int, AIParam(desc="The maximum number of associations to return.")] = 10,
+                                            offset: Annotated[int, AIParam(desc="Offset for pagination of results.")] = 0):
+            """Get a list of diseases associated with a phenotype."""
+            return self.api_call("phenotype-diseases", phenotype_id=phenotype_id, limit=limit, offset=offset)
+    
+    # get phenotype gene associations
+    @ai_function()
+    def get_phenotype_gene_associations(self,
+                                        phenotype_id: Annotated[str, AIParam(desc="The ontology identifier of the phenotype.")],
+                                        limit: Annotated[int, AIParam(desc="The maximum number of associations to return.")] = 10,
+                                        offset: Annotated[int, AIParam(desc="Offset for pagination of results.")] = 1):
+            """Get a list of genes associated with a phenotype."""
+            return self.api_call("phenotype-genes", phenotype_id=phenotype_id, limit=limit, offset=offset)
+        
+    # get entities
+    @ai_function()
+    def get_entities(self,
+                    ids: Annotated[List[str], AIParam(desc="The ontology identifier of the entity.")]):
+            """Get a single entity by its ontology identifier."""
+            return self.api_call("entity", ids=ids)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,214 +1,65 @@
-import streamlit as st
+# Example usage of StreamlitKani
+
+########################
+##### 0 - load libs
+########################
+
+
+# kani_streamlit imports
+import kani_utils.kani_streamlit_server as ks
+
+# for reading API keys from .env file
 import os
+import dotenv # pip install python-dotenv
+
+# kani imports
+from kani.engines.openai import OpenAIEngine
+
 from agents import PhenomicsAgent
+
+########################
+##### 1 - Configuration
+########################
+
+# read API keys .env file (e.g. set OPENAI_API_KEY=.... in .env and gitignore .env)
 import dotenv
-import logging
+dotenv.load_dotenv() 
 
-
-
-# Initialize session states
-def initialize_session_state():
-    if "logger" not in st.session_state:
-        st.session_state.logger = logging.getLogger(__name__)
-        st.session_state.logger.handlers = []
-        st.session_state.logger.setLevel(logging.INFO)
-        st.session_state.logger.addHandler(logging.StreamHandler())
-
-    st.session_state.setdefault("user_api_key", "")
-    st.session_state.setdefault("original_api_key", os.environ.get("OPENAI_API_KEY", None))  # Store the original API key
-    st.session_state.setdefault("show_function_calls", False)
-    st.session_state.setdefault("ui_disabled", False)
-    st.session_state.setdefault("lock_widgets", False)
-
-    greeting = """I'm the Phenomics Assistant, an AI chatbot with access to the [Monarch Inititive](https://monarchinitiative.org) biomedical knowledgebase. I can search for information on diseases, genes, and phenotypes. Here are some things you might try asking:
-    
-- What is the genetic basis of Cystic Fibrosis?
-- What symptoms are associated with Fanconi Anemia?
-
-Please note that I am a research preview, and this information should not be used for diagnoses, clinical decision making, or other medical applications."""
-
-    if "agents" not in st.session_state:
-        st.session_state.agents = {
-            "Phenomics Assistant": {
-                "agent": PhenomicsAgent("Phenomics Assistant", model="gpt-4-turbo-2024-04-09", openai_api_key=get_current_api_key_for_agent_use(), auto_summarize_buffer_tokens=10000),
-                "greeting": greeting,
-                "avatar": "ℹ️",
-                "user_avatar": "👤",
-            },
-            "Phenomics Assistant (GPT 3.5)": {
-                "agent": PhenomicsAgent("Phenomics Assistant (GPT 3.5)", model="gpt-3.5-turbo-0613", openai_api_key=get_current_api_key_for_agent_use(), auto_summarize_buffer_tokens=1000),
-                "greeting": greeting,
-                "avatar": "ℹ️",
-                "user_avatar": "👤",
-            }
+# initialize the application and set some page settings
+# parameters here are passed to streamlit.set_page_config, 
+# see more at https://docs.streamlit.io/library/api-reference/utilities/st.set_page_config
+# this function MUST be run first
+ks.initialize_app_config(
+    show_function_calls = True,
+    page_title = "Phenomics Assistant",
+    page_icon = "ℹ️", # can also be a URL
+    initial_sidebar_state = "expanded", # or "expanded"
+    menu_items = {
+            "Get Help": "https://github.com/monarch-initiative/phenomics-assistant",
+            "Report a Bug": "https://github.com/monarch-initiative/phenomics-assistant/issues",
+            "About": "Phenomics Assistant is built on GPT-4, Streamlit, zhudotexe/kani, hourfu/redlines, and oneilsh/kani-utils.",
         }
+)
 
-    st.session_state.setdefault("current_agent_name", list(st.session_state.agents.keys())[0])
+# define an engine to use (see Kani documentation for more info)
+engine = OpenAIEngine(os.environ["OPENAI_API_KEY"], model="gpt-4o")
+engine35 = OpenAIEngine(os.environ["OPENAI_API_KEY"], model="gpt-3.5-turbo-0125")
 
-    for agent in st.session_state.agents.values():
-        if "conversation_started" not in agent:
-            agent["conversation_started"] = False
-        if "messages" not in agent:
-            agent["messages"] = []
-
-def initialize_page():
-    st.set_page_config(
-        page_title="Phenomics Assistant",
-        page_icon="ℹ️",
-        layout="centered",
-        initial_sidebar_state="expanded",
-        menu_items={
-            "Get Help": "https://github.com/monarch-initiative/phenomics-assistant/issues",
-            "About": "This application is brought to you in part by the [Monarch Initiative](https://next.monarchinitiative.org).",
-        }
-    )
-
-# Get the current API key, either the user's or the default
-def get_current_api_key_for_agent_use():
-    key = st.session_state.user_api_key if st.session_state.user_api_key else st.session_state.original_api_key
-    if key is None:
-        key = "placeholder"
-    return key
-
-# Update agents with new API key
-def update_agents_api_key():
-    for agent in st.session_state.agents.values():
-        agent["agent"].set_api_key(get_current_api_key_for_agent_use())
-
-# Check if we have a valid API key
-def has_valid_api_key():
-    return bool(st.session_state.user_api_key) or bool(st.session_state.original_api_key)
+# We also have to define a function that returns a dictionary of agents to serve
+# Agents are keyed by their name, which is what the user will see in the UI
+def get_agents():
+    return {
+            "Phenomics Assistant (4o)": PhenomicsAgent(engine, prompt_tokens_cost = 0.01, completion_tokens_cost = 0.03),
+            "Phenomics Assistant (3.5)": PhenomicsAgent(engine35, prompt_tokens_cost = 0.0005, completion_tokens_cost = 0.0015),
+           }
 
 
-# Render chat message
-def render_message(message):
-    current_agent_avatar = st.session_state.agents[st.session_state.current_agent_name].get("avatar", None)
-    current_user_avatar = st.session_state.agents[st.session_state.current_agent_name].get("user_avatar", None)
-
-    if message.role == "user":
-        with st.chat_message("user", avatar = current_user_avatar):
-            st.write(message.content)
-
-    elif message.role == "system":
-        with st.chat_message("assistant", avatar="ℹ️"):
-            st.write(message.content)
-
-    elif message.role == "assistant" and not message.is_function_call:
-        with st.chat_message("assistant", avatar=current_agent_avatar):
-            st.write(message.content)
-
-    if st.session_state.show_function_calls:
-        if message.is_function_call:
-            with st.chat_message("assistant", avatar="🛠️"):
-                st.text(f"{message.func_name}(params = {message.func_arguments})")
-
-        elif message.role == "function":
-            with st.chat_message("assistant", avatar="✔️"):
-                st.text(message.content)
-
-    current_action = "*Thinking...*"
-
-    if message.is_function_call:
-        current_action = f"*Checking source ({message.func_name})...*"
-    elif message.role == "function":
-        current_action = f"*Evaluating result ({message.func_name})...*"
-
-    return current_action
-    
-# Handle chat input and responses
-def handle_chat_input():
-    if prompt := st.chat_input(disabled=st.session_state.lock_widgets, on_submit=lock_ui):  # Step 4: Add on_submit callback
-        agent = st.session_state.agents[st.session_state.current_agent_name]
-
-        # Continue with conversation
-        if not agent.get('conversation_started', False):
-            messages = agent['agent'].chat(prompt, yield_prompt_message=True)
-            agent['conversation_started'] = True
-        else:
-            messages = agent['agent'].chat(prompt, yield_prompt_message=True)
-
-        st.session_state.current_action = "*Thinking...*"
-        while True:
-            try:
-                with st.spinner(st.session_state.current_action):
-                    message = next(messages)
-                    agent['messages'].append(message)
-                    st.session_state.current_action = render_message(message)
-       
-                    session_id = st.runtime.scriptrunner.add_script_run_ctx().streamlit_script_run_ctx.session_id
-                    info = {"session_id": session_id, "message": message.model_dump(), "agent": st.session_state.current_agent_name}
-                    st.session_state.logger.info(info)
-            except StopIteration:
-                break
-
-        st.session_state.lock_widgets = False  # Step 5: Unlock the UI
-        st.rerun()
-
-def clear_chat_current_agent():
-    current_agent = st.session_state.agents[st.session_state.current_agent_name]
-    current_agent['conversation_started'] = False
-    current_agent['agent'].clear_history()
-    st.session_state.agents[st.session_state.current_agent_name]['messages'] = []
+# tell the app to use that function to create agents when needed
+ks.set_app_agents(get_agents)
 
 
-# Lock the UI when user submits input
-def lock_ui():
-    st.session_state.lock_widgets = True
+########################
+##### 3 - Serve App
+########################
 
-# Main Streamlit UI
-def main():
-    with st.sidebar:
-        # st.title("Settings")
-
-        agent_names = list(st.session_state.agents.keys())
-        current_agent_name = st.selectbox(label = "**Assistant**", 
-                                          options=agent_names, 
-                                          key="current_agent_name", 
-                                          disabled=st.session_state.lock_widgets, 
-                                          label_visibility="visible")
-        st.button(label = "Clear chat for current assistant", 
-                  on_click=clear_chat_current_agent, 
-                  disabled=st.session_state.lock_widgets)
-        st.checkbox("🛠️ Show calls to external tools", 
-                    key="show_function_calls", 
-                    disabled=st.session_state.lock_widgets)
-
-        st.markdown("---")
-
-        # Add user input for API key
-
-        user_key = st.text_input("Set API Key", 
-                                 value = st.session_state.user_api_key, 
-                                 max_chars=51, type="password", 
-                                 help = "Enter your OpenAI API key here to override the default provided by the app.", 
-                                 disabled=st.session_state.lock_widgets)
-        if user_key != st.session_state.user_api_key and len(user_key) == 51:
-            st.session_state.user_api_key = user_key
-            update_agents_api_key()
-            # write a label like "sk-...lk6" to let the user know a custom key is set and which one
-            st.write(f"Using API key: `{user_key[:3]}...{user_key[-3:]}`")
-
-
-    st.header(st.session_state.current_agent_name)
-
-    current_agent_avatar = st.session_state.agents[st.session_state.current_agent_name].get("avatar", None)
-    with st.chat_message("assistant", avatar = current_agent_avatar):
-        st.write(st.session_state.agents[st.session_state.current_agent_name]['greeting'])
-
-    for message in st.session_state.agents[st.session_state.current_agent_name]['messages']:
-        render_message(message)
-
-    # Check for valid API key and adjust chat input box accordingly
-    if has_valid_api_key():
-        handle_chat_input()
-    else:
-        st.chat_input(placeholder="Enter an API key to begin chatting.", disabled=True)
-
-
-
-# Main script execution
-if __name__ == "__main__":
-    initialize_page()
-    dotenv.load_dotenv()
-    initialize_session_state()
-    main()
+ks.serve_app()
